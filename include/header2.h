d16c7e890ca16b15690282eaaefde855b7747f05
sihyun-noh <idshtlgus@gmail.com>
2024-05-17 22:35:12 +0900

    Implement the first state mechin code

--- include/header2.h
+++ include/header2.h
@@ -1,8 +1,9 @@
 #ifndef HEADER2_H
 #define HEADER2_H
 
+typedef enum { IDLE, SELECTION, PAYMENT, DISPENSE } State;
+typedef enum { EVENT_SELECT, EVENT_PAY, EVENT_DISPENSE, EVENT_NONE } Event;
+
 void function2();
 
-#endif  // HEADER2_H
-
-
+#endif // HEADER2_H
--- src/main.c
+++ src/main.c
@@ -1,10 +1,47 @@
-#include <stdio.h>
 #include "header1.h"
+#include "header2.h"
+#include <stdio.h>
 
-int main() {
-    printf("Hello, world!\n");
-    printf("Addition result: %d\n", add(5, 3));
-    printf("Addition result: %d\n", add(5, 3));
-    return 0;
+State currentState = IDLE;
+
+void handleEvent(Event event) {
+  switch (currentState) {
+  case IDLE:
+    if (event == EVENT_SELECT) {
+      currentState = SELECTION;
+      printf("Transition to SELECTION state\n");
+    }
+    break;
+  case SELECTION:
+    if (event == EVENT_PAY) {
+      currentState = PAYMENT;
+      printf("Transition to PAYMENT state\n");
+    }
+    break;
+  case PAYMENT:
+    if (event == EVENT_DISPENSE) {
+      currentState = DISPENSE;
+      printf("Transition to DISPENSE state\n");
+    }
+    break;
+  case DISPENSE:
+    if (event == EVENT_NONE) {
+      currentState = IDLE;
+      printf("Transition to IDLE state\n");
+    }
+    break;
+  }
 }
 
+int main() {
+
+  printf("Hello, world!\n");
+  printf("Addition result: %d\n", add(5, 3));
+  handleEvent(EVENT_SELECT);   // IDLE -> SELECTION
+  handleEvent(EVENT_PAY);      // SELECTION -> PAYMENT
+  handleEvent(EVENT_DISPENSE); // PAYMENT -> DISPENSE
+  handleEvent(EVENT_NONE);     // DISPENSE -> IDLE
+
+  printf("Addition result: %d\n", add(5, 3));
+  return 0;
+}
